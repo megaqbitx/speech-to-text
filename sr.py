c11067c855384b32263b3579bb7eeb753ecd64d3
Juan Pablo Partridge <megaqbitx@gmail.com>
2023-10-10 09:12:05 -0300

    Save text into a file

--- sr.py
+++ sr.py
@@ -1,23 +1,34 @@
 
 import speech_recognition as sr
 
+
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
 
 # Reading Audio file as source
 # listening the audio file and store in audio_text variable
 
-with sr.AudioFile('conversation-1.wav') as source:
+
+with sr.AudioFile('intr-economia.wav') as source:
     
     audio_text = r.listen(source)
+
+
     
 # recognize_() method will throw a request error if the API is unreachable, hence using exception handling
     try:
         
         # using google speech recognition
         text = r.recognize_google(audio_text, language = "es-AR")
-        print('Converting audio into text...')
-        print(text)
+        
+        newFile = open('myfile.txt', "w")
+
+        newFile.write(text)
+
+        newFile.close()
+
+        print('The text has been generated!')
      
     except:
          print('Error')
+
